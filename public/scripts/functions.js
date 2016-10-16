867fc32d4664a480574bfb52316709238d9eaa07
Sindre <sndrem@live.no>
2016-10-16 07:41:20 +0200

    Gir beskjed til bruker hvis input er tomt

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -9,10 +9,13 @@ $(function() {
                 // check if search is empty
                 if(search.length === 0) {
                     // Search is empty, render message to user
+                    $("legend").html("Search cannot be empty");
                 } else {
                     // Search is not empty, proceed to check for data in the triple store
                 }
             });             
         }
     };
+
+    app.searchBtnClick();
 });
--- public/scripts/min/functions-min.js
+++ public/scripts/min/functions-min.js
@@ -1,1 +1,1 @@
-$(function(){var n={searchBtnClick:function(){$("#searchBtn").on("click",function(n){n.preventDefault();var c=$("#nameSearch").val();0===c.length})}}});
+$(function(){var n={searchBtnClick:function(){$("#searchBtn").on("click",function(n){n.preventDefault();var e=$("#nameSearch").val();0===e.length&&$("legend").html("Search cannot be empty")})}};n.searchBtnClick()});
